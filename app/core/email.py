0ee9460033100294ee210a02fa480d807aa91586
0188cs181047 <shivprataprathour20@gmail.com>
2026-03-27 23:11:37 +0530

    manage the email setup

--- app/core/email.py
+++ app/core/email.py
@@ -1,17 +1,19 @@
-from fastapi_mail import ConnectionConfig
-from pydantic import EmailStr
 import os
-from dotenv import load_dotenv
+from fastapi_mail import ConnectionConfig
 
-load_dotenv()
+MAIL_USERNAME = os.getenv("MAIL_USERNAME", "acteviashiv@gmail.com")
+MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", "jblmpidoyeyupijp")
+MAIL_FROM = os.getenv("MAIL_FROM", "acteviashiv@gmail.com")
+MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
+MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.gmail.com")
 
 conf = ConnectionConfig(
-    MAIL_USERNAME = os.getenv("MAIL_USERNAME"),
-    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD"),
-    MAIL_FROM = os.getenv("MAIL_FROM"),
-    MAIL_PORT = os.getenv("MAIL_PORT"),
-    MAIL_SERVER = os.getenv("MAIL_SERVER"),
-    MAIL_STARTTLS = True,
-    MAIL_SSL_TLS = False,
-    USE_CREDENTIALS = True
+    MAIL_USERNAME=MAIL_USERNAME,
+    MAIL_PASSWORD=MAIL_PASSWORD,
+    MAIL_FROM=MAIL_FROM,
+    MAIL_PORT=MAIL_PORT,
+    MAIL_SERVER=MAIL_SERVER,
+    MAIL_STARTTLS=True,
+    MAIL_SSL_TLS=False,
+    USE_CREDENTIALS=True,
 )
